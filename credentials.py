34c2fa1365feea19db7c456bf166ef993cb30cde
Thibault Degrande <thibaultdegrande@Thibaults-MacBook-Pro.local>
2020-02-29 16:33:17 +0100

    igrate to cloud

---
+++ credentials.py
@@ -0,0 +1,6 @@
+email_address = 'agendacalculator@gmail.com'
+email_password = 'VVDAlv34'
+email_server = 'imap.gmail.com'
+email_server_port = 993
+send_server = 'smtp.gmail.com'
+send_server_port = 587
