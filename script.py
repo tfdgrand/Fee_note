d5571c7d28666075217786b54861a15282bdbb23
tfdgrand <degrandethibault@gmail.com>
2020-06-02 17:02:12 +0200

    fix gcloud instance path in filename

--- script.py
+++ script.py
@@ -1,20 +1,17 @@
-'''#!/usr/bin/python3'''
+#!/usr/bin/python3
 
-import os
 import sys
 from src.mailing import EmailClass
 from src.processing import ProcessClass
 from assets import credentials
-from definitions import ASSET_PATH
 
 
 if __name__ == "__main__":
-    
    
     m = EmailClass(credentials.email_address, credentials.email_password, credentials.email_server, credentials.email_server_port, credentials.send_server, credentials.send_server_port)
     
     mail = m.login_mail_server()
-    file_attached = os.path.join(ASSET_PATH, "hour_report.xlsx")
+    file_attached = "hour_report.xlsx"
     count = 0
 
     try:
@@ -24,35 +21,32 @@ if __name__ == "__main__":
     
     if count == 0:
         print('No messages. Script should end here.')
-        sys.exit(0)
-    #End script: check: https://stackoverflow.com/questions/179369/how-do-i-abort-the-execution-of-a-python-script
+        sys.exit(0) #End script
     
     else:
         while count > 0:
-            try:
-                email_from_name, email_from_email_address, email_subject, msg = m.get_headers(mail)
 
-                if m.get_attachment(msg) is None:
-                    m.send_negative_response(email_from_email_address, email_subject, "attachment")
+            email_from_name, email_from_email_address, email_subject, msg = m.get_headers(mail)
+
+            if m.get_attachment(msg) is None:
+                m.send_negative_response(email_from_email_address, email_subject, "attachment")
+                m.delete_messages(mail)  # Inbox message (b'1') and Sent box!
+                count = int(m.check_mails(mail))
+            else:
+                icsfile = m.get_attachment(msg)
+                p = ProcessClass(icsfile)
+                event_list = p.read_data()
+                if event_list is None:
+                    m.send_negative_response(email_from_email_address, email_subject, "empty_calendar")
                     m.delete_messages(mail)  # Inbox message (b'1') and Sent box!
                     count = int(m.check_mails(mail))
-                else:
-                    icsfile = m.get_attachment(msg)
-                    p = ProcessClass(icsfile)
-                    event_list = p.read_data()
-                    if event_list is None:
-                        m.send_negative_response(email_from_email_address, email_subject, "empty_calendar")
-                        m.delete_messages(mail)  # Inbox message (b'1') and Sent box!
-                        count = int(m.check_mails(mail))
-                        continue
-                    eventTable, last, first = p.create_event_table(p.read_data())
-                    p.add_duration(eventTable, p.create_hour_table(eventTable)).to_excel(file_attached)
-                    p.edit_excel(file_attached, first, last)
-                    m.send_report(eventTable, file_attached, email_from_name, email_from_email_address, email_subject)
-                    m.delete_messages(mail) #Inbox message (b'1') and Sent box!
-                    count = int(m.check_mails(mail))
-                    p.delete_file(file_attached)
-            except:
-                print('This email could not be processed. Please check what goes wrong in the logs')
+                    continue
+                eventTable, last, first = p.create_event_table(p.read_data())
+                p.add_duration(eventTable, p.create_hour_table(eventTable)).to_excel(file_attached)
+                p.edit_excel(file_attached, first, last)
+                m.send_report(eventTable, file_attached, email_from_name, email_from_email_address, email_subject)
+                m.delete_messages(mail)  # Inbox message (b'1') and Sent box!
+                count = int(m.check_mails(mail))
+                p.delete_file(file_attached)
 
         m.log_out_mailserver(mail)
